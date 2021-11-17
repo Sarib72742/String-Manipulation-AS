a2833527a3f4fc75ddf7a67a9301a959ae65e9e2
Muhammad Sarib <93588162+Sarib72742@users.noreply.github.com>
2021-11-17 19:34:57 +0500

    AS

---
+++ Assignment/Assignment 7/both ends.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, FirstChars, LastChars As String
+        Dim spacepos, lenstr As Integer
+
+        str1 = ""
+        FirstChars = ""
+        LastChars = ""
+        spacepos = 0
+        lenstr = 0
+
+        Console.Write("please enter string: ")
+        str1 = Console.ReadLine
+        spacepos = InStr(str1, " ")
+        lenstr = Len(str1)
+        If lenstr <= 2 Then
+            Console.WriteLine(str1)
+        ElseIf spacepos = 0 Then
+            FirstChars = Left(str1, 2)
+            LastChars = Right(str1, 2)
+        Else
+            FirstChars = Left(str1, 2)
+            LastChars = Mid(str1, spacepos - 2, 2)
+        End If
+        Console.WriteLine("New string " & FirstChars & LastChars)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
