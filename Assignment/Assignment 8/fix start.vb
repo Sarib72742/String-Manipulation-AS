b77130d35a8231477b60c8b08fc8116806c982b2
Muhammad Sarib <93588162+Sarib72742@users.noreply.github.com>
2021-11-17 19:35:24 +0500

    AS

---
+++ Assignment/Assignment 8/fix start.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim str, nextStr, newStr, firstChar, thisChar, Char1 As String
+        Dim lenstr As Integer
+
+        str = ""
+        nextStr = ""
+        firstChar = ""
+        thisChar = ""
+        Char1 = ""
+        newStr = ""
+        lenstr = 0
+
+        Console.Write("Enter string: ")
+        str = Console.ReadLine
+        Console.Write("Enter a character which want to replace: ")
+        Char1 = Console.ReadLine
+
+        firstChar = Left(str, 1)
+        lenstr = Len(str)
+        nextStr = Right(str, lenstr - 1)
+        For count = 1 To Len(nextStr)
+            thisChar = Mid(nextStr, count, 1)
+            If thisChar = Char1 Then
+                newStr = newStr & "*"
+            Else
+                newStr = newStr & thisChar
+            End If
+        Next
+        Console.WriteLine("New string " & firstChar & newStr)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
