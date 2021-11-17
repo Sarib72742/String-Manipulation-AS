515258c4d131b6332ef1698d1e301b6f581c02f9
Muhammad Sarib <93588162+Sarib72742@users.noreply.github.com>
2021-11-17 19:27:05 +0500

    AS

---
+++ Assignment/Assignment 2/replace char .vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, Str2, mychar1, mychar2, currentchar As String
+        Dim Count As Integer
+
+        Str1 = ""
+        Str2 = ""
+        mychar1 = ""
+        mychar2 = ""
+        currentchar = ""
+        Count = 0
+
+        Console.Write("Enter String : ")
+        Str1 = Console.ReadLine
+
+        Console.Write("Enter character to remove : ")
+        mychar1 = Console.ReadLine
+
+        Console.Write("Enter character to replce : ")
+        mychar2 = Console.ReadLine
+
+        For Count = 1 To Len(Str1)
+            currentchar = Mid(Str1, Count, 1)
+            If currentchar = mychar1 Then
+                currentchar = mychar2
+            End If
+            Str2 = Str2 & currentchar
+        Next
+
+        Console.WriteLine("Final string = " & Str2)
+        Console.ReadKey()
+    End Sub
+
+End Module
