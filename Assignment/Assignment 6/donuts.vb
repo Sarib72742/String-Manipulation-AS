ce5c8042afae5e8c3f86577c259b5ef5a9508cbc
Muhammad Sarib <93588162+Sarib72742@users.noreply.github.com>
2021-11-17 19:34:23 +0500

    AS

---
+++ Assignment/Assignment 6/donuts.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim count As Integer
+
+        count = 0
+
+        Console.Write("Please enter no.of donuts: ")
+        count = Console.ReadLine
+
+        If count >= 0 And count <= 9 Then
+            Console.WriteLine("Number of Donuts: " & count)
+        Else
+            Console.WriteLine("Number of Donuts: Many ")
+        End If
+        Console.ReadKey()
+
+    End Sub
+
+End Module
