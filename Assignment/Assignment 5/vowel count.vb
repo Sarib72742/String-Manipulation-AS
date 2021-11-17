2738d75f86d9aa2f88b6b6132dc61d566e8e0f37
Muhammad Sarib <93588162+Sarib72742@users.noreply.github.com>
2021-11-17 19:33:56 +0500

    AS

---
+++ Assignment/Assignment 5/vowel count.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str, thischar As String
+        Dim Counter, vCount As Integer
+
+        Str = ""
+        thischar = ""
+        vCount = 0
+        Counter = 0
+        Const vowels = "aAeEiIoOuU"
+
+        Console.Write("Enter string to look for vowels: ")
+        Str = Console.ReadLine
+
+        For Counter = 1 To Len(Str)
+            thischar = Mid(Str, Counter, 1)
+            If InStr(vowels, thischar) > 0 Then
+                vCount = vCount + 1
+            End If
+        Next
+
+        Console.WriteLine("Number of vowels in the given string = " & vCount)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
