cb9516d48a4efc6fce200b17eb55f4af0eb38454
Muhammad Sarib <93588162+Sarib72742@users.noreply.github.com>
2021-11-17 19:33:26 +0500

    AS

---
+++ Assignment/Assignment 4/most char.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Highest, Count, count1, count2 As Integer
+        Dim str1, myChar, thisChar, newChar, countchar As String
+
+        str1 = ""
+        Highest = 0
+        Count = 0
+        count1 = 0
+        count2 = 0
+        myChar = ""
+        thisChar = ""
+        newChar = ""
+
+        Console.Write("Enter STRING: ")
+        str1 = Console.ReadLine
+
+        For count1 = 1 To Len(str1)
+            newChar = Mid(str1, count1, 1)
+            Count = 0
+            For count2 = 1 To Len(str1)
+                thisChar = Mid(str1, count2, 1)
+                If thisChar = newChar Then
+                    Count = Count + 1
+                End If
+            Next count2
+            If Count > Highest Then
+                Highest = Count
+                myChar = newChar
+            End If
+        Next count1
+        Console.WriteLine(myChar & " appeared " & Highest & " number of times.")
+        Console.ReadKey()
+    End Sub
+
+End Module
